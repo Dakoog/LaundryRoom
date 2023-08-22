bcde42a64672f72e5d8a5c563608cd3251b03180
Lenovo <taktoja@spoko.pl>
2023-08-22 10:08:52 +0200

    this is my sketch of Laundry designs

---
+++ src/Laundry.java
@@ -0,0 +1,28 @@
+public class Laundry {
+    // szablon
+    /*
+    classes - laundry, customer,order,  payment, order item
+    enum - service,  assortment orderStatus (placed,ready,realised) delivery (self-pickup , home delivery)
+           payment type (card, cash, blik)
+
+    laundry  methods: place order , deliver , markReady, pay
+     List array list (orders)
+     map hashmap (number, customer)
+
+     customer
+     fields: name, address
+
+     order
+     field customer
+     field status
+     field boolean (paid)
+     list array list (order item)
+     methods: markReady, markDelivered, summarize, markPaid
+
+ order item
+ fields: service, assortment, price, amount
+
+
+
+     */
+}
